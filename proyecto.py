e53b739854029fe059e25794e88655849534f2b5
EzequilePM <133610919+EzequilePM@users.noreply.github.com>
2026-06-15 01:43:28 +0000

    Se creó vistas.py y se movió allí el código relacionado con la visualización en Streamlit

--- proyecto.py
+++ proyecto.py
@@ -1,6 +1,5 @@
 import csv
-import streamlit as st
-import matplotlib.pyplot as plt
+import vistas
 
 
 #toma lista defilas del excel y devuelve la de la posicion 1
@@ -42,29 +41,6 @@ def main():
         #pregunta6
     var1 = mas_apariciones(ciudades)  # var1 usa la funcion mas_apariciones con la lista ciudades y 
                                       #devuelve libreria con ciudad:apariciones
-    tab1, tab2, tab3 = st.tabs(["¿En qué ciudades se hicieron más mediciones?", "Pregunta siguiente", "Pregunta siguiente"])
-    with tab1:
-        st.header("Gráfica de mediciones")
-
-        data = var1
-        names = list(data.keys())
-        values = list(data.values())
-        fig, ax = plt.subplots(figsize=(20, 16))
-        ax.bar(names, values)
-        ax.tick_params(axis = "x",labelcolor='black', rotation= 270, labelsize=20 )     # rota los nombres hacia abajo y agranda la letra de los valores en x
-        ax.tick_params(axis = "y", labelsize=20 , labelcolor='black'  ) #cambia el tamaño de los valores en y #agrege labelcolor='black', solo para tener en cuenta el cambio de color
-        maximo = max(values) # da el valor maximo de values
-        y_lista = list(range(0, maximo + 40, 40))# genera una lista de valores desde 0 hasta el maximo mas 30
-        y_lista.append(maximo)# a la lista y_lista le agrega el valor del maximo
-        ax.set_yticks(y_lista)#los valores que se muestran en y 
-        plt.tight_layout()      # evita que se corten
-        st.pyplot(fig)
-    with tab2:
-        st.header("Representación")
-        st.image("https://static.streamlit.io/examples/dog.jpg", width=1080)
-    with tab3:
-        st.header("Representacion")
-        st.image("https://static.streamlit.io/examples/owl.jpg", width=1080)
-
+    vistas.mostrar_vistas({1:var1})
 if __name__=="__main__":
     main()
---
+++ vistas.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+
+def vista_pregunta_1(tab, data: dict) -> None:
+    """
+    Dado un objeto TabContainer y un diccionario con los nombres de las ciudades y un número entero.
+    Genera el tab correspondiente a la pregunta: ¿En qué ciudades se hicieron más mediciones?
+    """
+    with tab:
+        st.header("Gráfica de mediciones")
+
+        names = list(data.keys())
+        values = list(data.values())
+        fig, ax = plt.subplots(figsize=(20, 16))
+        ax.bar(names, values)
+        ax.tick_params(axis = "x",labelcolor='black', rotation= 270, labelsize=20 )     # rota los nombres hacia abajo y agranda la letra de los valores en x
+        ax.tick_params(axis = "y", labelsize=20 , labelcolor='black'  ) #cambia el tamaño de los valores en y #agrege labelcolor='black', solo para tener en cuenta el cambio de color
+        maximo = max(values) # da el valor maximo de values
+        y_lista = list(range(0, maximo + 40, 40))# genera una lista de valores desde 0 hasta el maximo mas 30
+        y_lista.append(maximo)# a la lista y_lista le agrega el valor del maximo
+        ax.set_yticks(y_lista)#los valores que se muestran en y 
+        plt.tight_layout()      # evita que se corten
+        st.pyplot(fig)
+
+#def vista_pregunta_2(tab, data):
+#    with tab:
+#        st.header("Representación")
+#        st.image("https://static.streamlit.io/examples/dog.jpg", width=1080)
+
+def mostrar_vistas(respuestas: dict) -> None:
+    """
+    La función define todos los tabs que se mostraran en la vista web.
+    La misma requiere un diccionario formado por:
+    key (int): Número de pregunta.
+    value (any): Los datos que deven mostrarse (Según el contrato de cada vista).
+    """
+    tab1, tab2, tab3 = st.tabs(["¿En qué ciudades se hicieron más mediciones?", "Pregunta siguiente", "Pregunta siguiente"])
+    vista_pregunta_1(tab1, respuestas[1])
+    #vista_pregunta_2(tab2, None)
