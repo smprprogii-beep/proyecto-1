a2fde48c0558eac3ceb04e410112dd0dee5e66d4
lautaromandingorra <142627437+lautaromandingorra@users.noreply.github.com>
2026-06-23 19:34:22 +0000

    agregue test para las funciones ciudades_superiores_a y adaptador_dioxid

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from proyecto import obtener_fecha, obtener_campos
+from consultas_al_informe import adaptador_dioxido, ciudades_superiores_a
 
 def test_obtener_fecha():
 
@@ -11,3 +12,56 @@ def test_obtener_campos():
     assert obtener_campos(registro, ["City", "Latitude", "Longitude"]) == ["Mexico City",19.4326,-99.1332]
     assert obtener_campos(registro, ["City", "Latitude", "Timestamp", "Longitude"]) == ["Mexico City",19.4326, "2026-02-15T13:00", -99.1332]
     assert obtener_campos(registro, ["City"]) == ["Mexico City"]
+
+
+def test_adaptador_dioxido():
+    informe = {
+        "Rosario": {"Nitrogen_Dioxide_ug_m3": 18.5 },
+        "Córdoba": {"Nitrogen_Dioxide_ug_m3": 27.0},
+        "Santa Fe": {"Nitrogen_Dioxide_ug_m3": 41.3}
+    }
+    esperado = {
+        "Rosario": 18.5,
+        "Córdoba": 27.0,
+        "Santa Fe": 41.3
+    }
+    assert adaptador_dioxido(informe) == esperado
+    assert adaptador_dioxido({}) == {}
+
+def test_ciudades_superiores_a():
+    datos = {
+        "Rosario": 18,
+        "Córdoba": 35,
+        "Santa Fe": 50,
+        "Mendoza": 12
+    }
+    esperado = {
+        "Córdoba": 35,
+        "Santa Fe": 50
+    }
+    assert ciudades_superiores_a(datos, 30) == esperado
+
+    datos = {
+        "Rosario": 10,
+        "Córdoba": 15,
+        "Santa Fe": 20
+    }
+    assert ciudades_superiores_a(datos, 50) == {}
+
+    datos = {
+        "Rosario": 40,
+        "Córdoba": 55,
+        "Santa Fe": 80
+    }
+    assert ciudades_superiores_a(datos, 30) == datos
+
+    datos = {
+        "Rosario": 30,
+        "Córdoba": 29.9,
+        "Santa Fe": 30.1
+    }
+    esperado = {
+        "Rosario": 30,
+        "Santa Fe": 30.1
+    }
+    assert ciudades_superiores_a(datos, 30) == esperado
